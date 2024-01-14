808c006a4bfc49030ae1d117d2bba174ed5ca408
FranciscoTurro <fturro@outlook.com>
2024-01-13 22:46:27 -0300

    initial commit

---
+++ code/win32_tedzukuri.cpp
@@ -0,0 +1,10 @@
+#include <windows.h>
+
+int CALLBACK WinMain(
+    HINSTANCE hInst,
+    HINSTANCE hInstPrev,
+    PSTR cmdline,
+    int cmdshow)
+{
+    return 0;
+}
---
+++ misc/shell.bat
@@ -0,0 +1,4 @@
+@echo off
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build\vcvarsall" x64
+set path=D:\tedzukuri\misc;%path%;
+code .
